1805e39eb63a3e51b57aa35278b321eaa78c0f35
mmagnuski <mmagnuski@swps.edu.pl>
2015-12-04 01:31:20 +0100

    add project manager to Olga proj

--- proj_Olga_N170.m
+++ proj_Olga_N170.m
@@ -11,7 +11,21 @@ braintools;
 eegDb;
 eeg_path('add');
 
+p = project();
+
+p.pth('root', {'D:\Dropbox\CURRENT PROJECTS\N170 Olga', ...
+    'C:\Users\Ola\Dropbox\N170 Olga'});
+p.pth('code', fullfile(p('root'), 'code'));
+p.pth('data', fullfile(p('root'), 'SET'));
+
+fls = dir(fullfile(p('data'), '*.set'));
+
+% add paths
+p.addp('code');
+
+
 % set of anonymous funs:
 cnt = @(x) x(1):x(2);
+dtpth = p('data');
 loads = @(x) pop_loadset(fullfile(dtpth, x));
 
