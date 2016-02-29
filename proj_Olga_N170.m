241e3fed3e5b6a191431dfe7896f00a7a4ba9e86
mmagnuski <mmagnuski@swps.edu.pl>
2016-02-29 17:46:56 +0100

    fix bug

--- proj_Olga_N170.m
+++ proj_Olga_N170.m
@@ -31,6 +31,6 @@ cnt = @(x) x(1):x(2);
 dtpth = p('data');
 loads = @(x) pop_loadset(fullfile(dtpth, x));
 try %#ok<TRYNC>
-EEG = ld(fls{1});
-msk = @(x, m) maskitsweet(x, m, 'Time', EEG.times);
+    EEG = loads(fls(1).name);
+    msk = @(x, m) maskitsweet(x, m, 'Time', EEG.times);
 end
