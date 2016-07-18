1a76e8a9af8f447c5e41c4f9e1c62044e27c42e9
mmagnuski <mmagnuski@swps.edu.pl>
2016-07-18 20:38:34 +0200

    update proj Olga

--- proj_Olga_N170.m
+++ proj_Olga_N170.m
@@ -1,7 +1,8 @@
 % proj_OlgaN170
 has_btls = which('braintools');
 if ~has_btls
-    btls = {'D:\DATA\GIT\braintools\', 'E:\Programy\braintools', };
+    btls = {'D:\DATA\GIT\braintools\', 'E:\Programy\braintools', ...
+        'C:\proj\src\braintools'};
     exists_dir = cellfun(@(x) isdir(x), btls);
     btls = btls(exists_dir);
     btls = btls{1};
@@ -16,6 +17,7 @@ eeg_path('add');
 p = project();
 
 p.pth('root', {'D:\Dropbox\CURRENT PROJECTS\N170 Olga', ...
+    'C:\Users\swps\Dropbox\CURRENT PROJECTS\N170 Olga', ...
     'C:\Users\Ola\Dropbox\N170 Olga', 'D:\Dropbox\N170 Olga'});
 p.pth('code', fullfile(p('root'), 'code'));
 p.pth('data', fullfile(p('root'), 'SET'));
@@ -24,7 +26,9 @@ fls = dir(fullfile(p('data'), '*.set'));
 
 % add paths
 addpath(genpath(p('code')));
-addpath('D:\programy\fieldtrip');
+ftrp_pth = get_valid_path({...
+    'D:\programy\fieldtrip', 'C:\proj\src\fieldtrip'});
+addpath(ftrp_pth);
 
 
 % set of anonymous funs:
